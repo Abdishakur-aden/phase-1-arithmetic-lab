8efc3304b4a306ebacec796059dda95247c97a41
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-03-19 16:03:50 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 =31;
+const num2 =2;
+let multiply=(num1 * num2);
 
-// Write your code here
+
+const random = Math.floor(Math.random()*10);
+
+
+const num3=12;
+let num4=8;
+const mod = (num3 % num4);
+
+
+const max= Math.max(11, 4, 18, 9, 20, 6);
+
+
+
